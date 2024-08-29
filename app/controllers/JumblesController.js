c92c05f3b36e47d9ccac5c61b4b4ee7d8b1b77ac
Elijah SasserCollins <ElijahJSasserCollins@gmail.com>
2024-08-29 14:41:50 -0600

    drawing Jumbles

--- app/controllers/JumblesController.js
+++ app/controllers/JumblesController.js
@@ -1,7 +1,20 @@
+import { AppState } from "../AppState.js"
+import { setHTML } from "../utils/Writer.js"
+
 export class JumblesController {
 
   constructor() {
     console.log(`Jumbles controller is live `)
+
+    this.drawJumbles()
+  }
+
+
+  drawJumbles() {
+    const jumbles = AppState.jumbles
+    let JumblesHTML = ``
+    jumbles.forEach(jumble => JumblesHTML += jumble.JumbleHTMLTemplate)
+    setHTML(`jumblesList`, JumblesHTML)
   }
 
 }
