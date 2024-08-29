407f99541313a18f27569d96f1180332ce9c0c2b
Elijah SasserCollins <ElijahJSasserCollins@gmail.com>
2024-08-29 13:39:43 -0600

    cleaned house

--- app/App.js
+++ app/App.js
@@ -1,10 +1,9 @@
 import { router } from './router-config.js';
-import { HomeController } from './controllers/HomeController.js';
 const USE_ROUTER = false
 
 class App {
 
-  HomeController = new HomeController()
+
 
   constructor() {
     if (USE_ROUTER) {
